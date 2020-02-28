14ffbc5b9adb46f958b0552a062dafcb1f103a18
NukeBird <nukebird.dev@gmail.com>
2020-02-28 18:50:47 +0300

    Oh hello there

---
+++ some_cool_source.cpp
@@ -0,0 +1,10 @@
+#include "some_cool_source.h"
+#include <iostream>
+
+namespace AwesomeNamespace
+{
+    void say(const std::string& text)
+    {
+        std::cout << text << std::endl;
+    }
+};
---
+++ some_cool_source.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string> 
+
+namespace AwesomeNamespace
+{
+    void say(const std::string& text);
+};
